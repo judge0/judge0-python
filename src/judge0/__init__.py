3080ac3480d7a0bab8cfcc99949ce6def80ad08e
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2025-07-26 16:58:55 +0200

    Prepare for release 0.0.4

--- src/judge0/__init__.py
+++ src/judge0/__init__.py
@@ -29,7 +29,7 @@ from .filesystem import File, Filesystem
 from .retry import MaxRetries, MaxWaitTime, RegularPeriodRetry
 from .submission import Submission
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.0.4"
 
 __all__ = [
     "ATD",
